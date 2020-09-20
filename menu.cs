69cb5f4eadde9deb7e51a52e48c694db38cfd496
fatima1235 <62561919+fatima1235@users.noreply.github.com>
2020-09-20 14:55:18 -0600

    Create menu.cs

---
+++ menu.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculadora
+{
+    class menu
+    {
+
+        private int op = 0;
+        public int ListaMenu()
+        {
+            Console.WriteLine("Bienvenido a ImpCalc. Seleccione la opción que desee.");
+            Console.WriteLine("1.- Calcular IVA");
+            Console.WriteLine("2.- Calcular ISR");
+            Console.WriteLine("3.- Calcular Bono 14");
+            Console.WriteLine("4.- Calcular Aguinaldo");
+            Console.WriteLine("5.- Salir");
+            Console.Write("Ingrese el numero de la opcion que desee: ");
+            op = int.Parse(Console.ReadLine());
+            return op;
+        }
+    }
+}
